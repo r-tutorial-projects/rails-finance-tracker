95993812cd638c44a779eb48ab7508fa6ae911e8
Ruben Vitt <dev@rubeen.dev>
2020-05-03 13:23:38 +0200

    feat(finance-tracker): add stocks list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,22 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  MAX_STOCKS = 10
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def stock_already_present?(ticker)
+    !stocks.where(ticker: ticker).empty?
+  end
+
+  def under_stock_limit?
+    stocks.count < MAX_STOCKS
+  end
+
+  def can_track_stock?(ticker)
+    !stock_already_present?(ticker) && under_stock_limit?
+  end
 end
